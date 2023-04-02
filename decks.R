a2cff7de5f9a055b9b34f510a50f51249dde5856
AlicedeSampaioKalkuhl <desampaa@tcd.ie>
2023-04-02 23:22:53 +0100

    02.04.2023

--- decks.R
+++ decks.R
@@ -6,31 +6,31 @@ decks<-data.frame(structure(list(deck=character(),commander=character(),power=fa
 decks<-decks%>%#priority 7: Tournament cEDH
   add_row(deck="Obscura Doomsday",commander="Toluz",power="7",played=11,won=5,draw=1,lost=5,priority="8",W=T,U=T,B=T,R=F,G=F,artist="Donato Giancola",plane1='New Capenna',plane2=NA)%>% 
   add_row(deck="Sultai Mean Girls",commander="Sidisi",power="7",played=4,won=2,draw=0,lost=2,priority="8",W=F,U=T,B=T,R=F,G=T,artist="Karl Kopinski",plane1='Tarkir',plane2=NA)%>%
-  add_row(deck="The Weatherlight is Under Construction",commander="Jhoira",power="7",played=5,won=0,draw=0,lost=5,priority="8",W=F,U=T,B=F,R=T,G=F,artist="Lisa Heidhoff",plane1='Dominaria',plane2=NA)%>%
+  add_row(deck="The Weatherlight is Under Construction",commander="Jhoira",power="7",played=8,won=0,draw=0,lost=8,priority="8",W=F,U=T,B=F,R=T,G=F,artist="Lisa Heidhoff",plane1='Dominaria',plane2=NA)%>%
   add_row(deck="Underworld Heist",commander="Cormela",power="7",played=1,won=0,draw=0,lost=1,priority="8",W=F,U=T,B=T,R=T,G=F,artist="Bram Sels",plane1='New Capenna',plane2=NA)%>%# priority 6: LGS high power 
-  add_row(deck="Nephalia Nightmares",commander="Jeleva",power="6",played=10,won=5,draw=0,lost=5,priority="7",W=F,U=T,B=T,R=T,G=F,artist="Cynthia Sheppard",plane1='Innistrad',plane2=NA)%>% 
+  add_row(deck="Nephalia Nightmares",commander="Jeleva",power="6",played=12,won=5,draw=1,lost=6,priority="7",W=F,U=T,B=T,R=T,G=F,artist="Cynthia Sheppard",plane1='Innistrad',plane2=NA)%>% 
   add_row(deck="Remember the Fallen",commander="Rayami",power="6",played=2,won=0,draw=0,lost=2,priority="6",W=F,U=T,B=T,R=F,G=T,artist="Kieran Yanner",plane1='Zendikar',plane2=NA) %>%
   add_row(deck="Reyhan and the Merfolks",commander="Thrasios & Reyhan",power="6",played=8,won=3,draw=0,lost=5,priority="7",W=F,U=T,B=T,R=F,G=T,artist="Josu Hernaiz,Chris Rallis",plane1='Theros',plane2='Tarkir') %>%
   # priority 5: LGS low power 
   add_row(deck="Astelle's Maze",commander="Nine-Fingers Keene",power="5",played=2,won=1,draw=0,lost=1,priority="6",W=F,U=T,B=T,R=F,G=T,artist="Nils Hamm",plane1='Forgotten Realms',plane2=NA)%>%
   add_row(deck="Bi Disasters",commander="Kamber & Laurine",power="5",played=2,won=1,draw=0,lost=1,priority="6",W=F,U=F,B=T,R=T,G=F,artist="Andrey Kuzinskiy,Andrey Kuzinskiy",plane1='Innistrad',plane2='Innistrad')%>%
-  add_row(deck="Clueing for Looks",commander="Katilda & Lier",power="6",played=2,won=0,draw=0,lost=0,priority="2",W=T,U=T,B=F,R=F,G=T,artist="Lie Setiawan",plane1='Innistrad',plane2=NA)%>% 
+  add_row(deck="Clueing for Looks",commander="Katilda & Lier",power="6",played=2,won=0,draw=0,lost=0,priority="2",W=T,U=T,B=F,R=F,G=T,artist="Lie Setiawan",plane1='Innistrad',plane2='Innistrad')%>% 
   add_row(deck="Descend Into Madness",commander="Anhelo",power="5",played=2,won=1,draw=0,lost=1,priority="6",W=F,U=T,B=T,R=T,G=F,artist="Marta Nael",plane1='New Capenna',plane2=NA) %>%
-  add_row(deck="Elspeth of the Nine Realms",commander="Quintorius",power="5",played=2,won=0,draw=0,lost=2,priority="1",W=T,U=F,B=F,R=T,G=F,artist="Bryan Sola",plane1='Strixhaven',plane2=NA)%>%
+  add_row(deck="Elspeth of the Nine Realms",commander="Quintorius",power="5",played=2,won=0,draw=0,lost=2,priority="1",W=T,U=F,B=F,R=T,G=F,artist="Bryan Sola",plane1='Arcavios',plane2=NA)%>%
   add_row(deck="Ethics Violations",commander="Volrath",power="6",played=3,won=1,draw=0,lost=2,priority="6",W=F,U=T,B=T,R=F,G=T,artist="Heonhwa Choe",plane1='Dominaria',plane2=NA)%>%
   add_row(deck="Exalted Glimpses",commander="Ayesha",power="5",played=2,won=2,draw=0,lost=2,priority="6",W=T,U=T,B=F,R=F,G=F,artist="Aurore Folny",plane1='Dominaria',plane2=NA)%>%
   add_row(deck="Everchosen Snow",commander="Najeela",power="5",played=2,won=0,draw=0,lost=2,priority="6",W=T,U=T,B=T,R=T,G=T,artist="Antonio José Manzanedo",plane1='Kylem',plane2=NA)%>%
   add_row(deck="Failed Parley",commander="Phabine",power="5",played=5,won=2,draw=0,lost=3,priority="6",W=F,U=T,B=T,R=F,G=T,artist="Ryan Pancoast",plane1='New Capenna',plane2=NA)%>%
   add_row(deck="Gifts to the Table",commander="Parnesse",power="6",played=9,won=0,draw=1,lost=8,priority="6",W=F,U=T,B=T,R=T,G=F,artist="Svetlin Velinov",plane1='New Capenna',plane2=NA)%>%
   add_row(deck="Graveyard of Nightmares",commander="Beamtown Bullies",power="5",played=2,won=0,draw=0,lost=2,priority="6",W=F,U=F,B=T,R=T,G=T,artist="Tyler Jacobson",plane1='New Capenna',plane2=NA)%>%
+  add_row(deck="Illusions of Property",commander="Rubinia",power="5",played=1,won=0,draw=0,lost=1,priority="6",W=T,U=T,B=F,R=F,G=T,artist="Cynthia Sheppard",plane1='Unknown',plane2=NA)%>%
   add_row(deck="Keep People Humble",commander="Ivy",power="5",played=5,won=1,draw=0,lost=4,priority="6",W=F,U=T,B=F,R=F,G=T,artist="Evyn Fong",plane1='Dominaria',plane2=NA) %>% 
-  add_row(deck="Let Me Borrow This",commander="Rubinia",power="5",played=1,won=0,draw=0,lost=1,priority="6",W=T,U=T,B=F,R=F,G=T,artist="Cynthia Sheppard",plane1='Unknown',plane2=NA)%>%
   add_row(deck="Let's All Work Together",commander="Sen Triplets",power="6",played=6,won=0,draw=0,lost=6,priority="6",W=T,U=T,B=T,R=F,G=F,artist="Greg Staples",plane1='Alara',plane2=NA)%>%
   add_row(deck="Off To See The Wizard",commander="Inalla",power="5",played=5,won=0,draw=0,lost=5,priority="6",W=F,U=T,B=T,R=T,G=F,artist="Yongjae Choi",plane1='Unknown',plane2=NA)%>%
   add_row(deck="Party of Two",commander="Minthara",power="5",played=4,won=1,draw=0,lost=3,priority="6",W=T,U=F,B=T,R=F,G=F,artist="Evyn Fong",plane1='Forgotten Realms',plane2=NA)%>%
   add_row(deck="Point Mutation",commander="Otrimi",power="5",played=3,won=1,draw=0,lost=2,priority="6",W=T,U=T,B=F,R=F,G=T,artist="Victor Adame Minguez",plane1='Ikoria',plane2=NA) %>%
   add_row(deck="True Love",commander="Regna & Krav",power="5",played=2,won=0,draw=0,lost=2,priority="6",W=T,U=F,B=T,R=F,G=F,artist="Randy Vargas,Randy Vargas",plane1='Kylem',plane2='Kylem')%>%  
-  add_row(deck="Where Are My Lesbians?",commander="Ikra & Kydele",power="5",played=0,won=0,draw=0,lost=0,priority="6",W=F,U=T,B=T,R=F,G=T,artist="Josu Hernaiz,Bastien L. Deharme",plane1='Theros',plane2='Tarkir')%>%
+  add_row(deck="Where Are My Lesbians?",commander="Zimone and Dina",power="5",played=0,won=0,draw=0,lost=0,priority="6",W=F,U=T,B=T,R=F,G=T,artist="Lie Setiawan,Lie Setiawan",plane1='Arcavios',plane2='Arcavios')%>%
   add_row(deck="You Should Be Nicer",commander="Atla",power="6",played=8,won=1,draw=0,lost=7,priority="6",W=T,U=F,B=F,R=T,G=T,artist="Ekaterina Burmak",plane1='Ixalan',plane2=NA)%>% 
   # priority 5: Oathbreaker 
   add_row(deck="Don't Worry About It",commander="Lukka",power="5",played=0,won=0,draw=0,lost=0,priority="5",W=F,U=F,B=F,R=T,G=T,artist="Chase Stone",plane1='Ikoria',plane2=NA) %>%
@@ -55,9 +55,9 @@ decks<-decks%>%#priority 7: Tournament cEDH
   add_row(deck="Kept in Suspense",commander="Illuna",power="5",played=2,won=1,draw=0,lost=1,priority="2",W=F,U=T,B=F,R=T,G=T,artist="Chris Rahn",plane1='Ikoria',plane2=NA)%>%
   add_row(deck="Jeskai",commander="Shu Yun",power="4",played=1,won=0,draw=0,lost=1,priority="2",W=T,U=T,B=F,R=T,G=F,artist="David Gaillet",plane1='Tarkir',plane2=NA)%>%
   add_row(deck="Legion of Blood",commander="Licia",power="6",played=4,won=2,draw=0,lost=2,priority="2",W=T,U=F,B=T,R=T,G=F,artist="Magali Villeneuve",plane1='Unknown',plane2=NA)%>%
-  add_row(deck="Necrowarfare",commander="Gisa and Geralf",power="5",played=4,won=2,draw=0,lost=2,priority="2",W=F,U=T,B=T,R=F,G=F,artist="Karla Ortiz",plane1='Innistrad',plane2=NA)%>%
+  add_row(deck="Necrowarfare",commander="Gisa and Geralf",power="5",played=4,won=2,draw=0,lost=2,priority="2",W=F,U=T,B=T,R=F,G=F,artist="Aaron J. Riley",plane1='Innistrad',plane2='Innistrad')%>%
   add_row(deck="Phyrexian Hippos",commander="Perrie with Keruga",power="6",played=6,won=2,draw=0,lost=4,priority="2",W=T,U=T,B=F,R=F,G=T,artist="Joshua Raphael,Dan Scott",plane1='New Capenna',plane2=NA) %>%
-  add_row(deck="Prismari for Life",commander="Galazeth Prismari",power="5",played=2,won=1,draw=0,lost=1,priority="2",W=F,U=T,B=F,R=T,G=F,artist="Iain McCaig",plane1='Strixhaven',plane2=NA)%>%
+  add_row(deck="Prismari for Life",commander="Galazeth Prismari",power="5",played=2,won=1,draw=0,lost=1,priority="2",W=F,U=T,B=F,R=T,G=F,artist="Iain McCaig",plane1='Arcavios',plane2=NA)%>%
   add_row(deck="Spores on the Playground",commander="Tana & Keskit",power="5",played=1,won=0,draw=0,lost=1,priority="2",W=F,U=T,B=T,R=F,G=T,artist="Magali Villeneuve,Yongjae Choi",plane1='Unknown',plane2='New Phyrexia') %>% 
   add_row(deck="Stegosaurus Combat Behaviour",commander="Kalamax",power="2",played=6,won=2,draw=0,lost=4,priority="2",W=F,U=T,B=F,R=T,G=T,artist="Nicholas Gregory",plane1='Ikoria',plane2=NA) %>%
  #priority 1: unsleeved and unfinished
@@ -76,10 +76,10 @@ decks<-decks%>%#priority 7: Tournament cEDH
   add_row(deck="Knights of the Shire",commander="Mazzy",power="5",played=0,won=0,draw=0,lost=0,priority="1",W=T,U=F,B=F,R=T,G=T,artist="Justyna Dura",plane1='Forgotten Realms',plane2=NA)%>%
   add_row(deck="Creteaceous Beasts",commander="Nethroi with Kaheera",power="5",played=0,won=0,draw=0,lost=0,priority="1",W=T,U=F,B=T,R=F,G=T, artist="Slawomir Maniak,Ryan Pancoast",plane1='Ikoria',plane2=NA)%>%
   add_row(deck="Pride was a Riot",commander="Kynaios & Tiro",power="5",played=0,won=0,draw=0,lost=0,priority="1",W=T,U=T,B=F,R=T,G=T,artist="Willian Murai",plane1='Theros',plane2=NA)%>%
-  add_row(deck="Release the Gremlins",commander="Wasitora",power="5",played=0,won=0,draw=0,lost=0,priority="1",W=F,U=F,B=T,R=T,G=T,artist="Cynthia Sheppard",plane1='Dominaria',plane2=NA) %>%
+  add_row(deck="Release the Gremlins",commander="Gimbal",power="5",played=0,won=0,draw=0,lost=0,priority="1",W=F,U=T,B=F,R=T,G=T,artist="Fajareka Setiawan",plane1='Kaladesh',plane2=NA) %>%
   add_row(deck="Rolling Blackouts",commander="Yidris",power="5",played=3,priority="2",W=F,U=T,B=T,R=T,G=T,artist="Karl Kopinski",plane1='Alara',plane2=NA) %>%
   add_row(deck="Runic Lore",commander="Gretchen",power="9",played=0,won=0,draw=0,lost=0,priority="1",W=F,U=T,B=F,R=T,G=T,artist="Mila Pesic",plane1='Forgotten Realms',plane2=NA)%>%
-  add_row(deck="Guardian of the Lost",commander="Ghave",power="5",played=0,won=0,draw=0,lost=0,priority="1",W=T,U=F,B=T,R=F,G=T,artist="James Paick",plane1='Unknown',plane2=NA)%>%
+  add_row(deck="Guardian of the Lost",commander="Ephara",power="5",played=0,won=0,draw=0,lost=0,priority="1",W=T,U=T,B=F,R=F,G=F,artist="Eric Deschamps",plane1='Theros',plane2=NA)%>%
   add_row(deck="Say Please",commander="Ertai",power="5",played=0,won=0,draw=0,lost=0,priority="1",W=T,U=T,B=T,R=F,G=F,artist="Sidharth Chaturvedi",plane1='Dominaria',plane2=NA)%>%
   add_row(deck="Unspeakable Horrors",commander="Kodama & Ghost of Ramirez",power="5",played=0,won=0,draw=0,lost=0,priority="1",W=F,U=T,B=F,R=F,G=T,artist="Daarken,Grzegorz Rutkowski",plane1='Kamigawa',plane2='Dominaria')%>%  
   add_row(deck="Women in STEM",commander="Sophina & Cecily",power="5",played=0,won=0,draw=0,lost=0,priority="1",W=T,U=T,B=T,R=T,G=F,artist="Bartek Fedyczak,Anastasia Balakchina",plane1='Innistrad',plane2='Innistrad')
