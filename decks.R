7b25ad4be5cb6ba6d55a1d46d9e0ea5f47945ade
AlicedeSampaioKalkuhl <alice@quantumlandbooks.com>
2024-03-28 23:00:41 +0100

    28.03.2024

--- decks.R
+++ decks.R
@@ -4,7 +4,7 @@ library(viridis)
 library(ggrepel)
 
 # If you don't wan to edit your decks from within R, you can import any table with the same column names. 
-decks<-read.csv("Decks.csv",quote="",stringsAsFactors=F,header=T)
+decks<-read.csv("Decks.csv",quote="",stringsAsFactors=F,header=T) %>% arrange(priority)
 
 # colour distibution
 W<-decks%>%filter(W==TRUE)%>%count()%>%pull(n)
@@ -17,15 +17,20 @@ identity<-c("W","U","B","R","G")
 colours<-cbind(colour,identity)
 colours<-as.data.frame(colours)
 colours<-colours%>%mutate(colour=as.numeric(colour))
-col<-colours%>%ggplot(aes(x=fct_inorder(identity),colour))+
-          geom_bar(stat="identity")+
-          xlab("Colour Identity")+
-          ylab("Count")
+col<-colours%>%ggplot(aes(x="",colour,fill=identity))+
+          geom_bar(stat="identity", width=1)+
+          theme(axis.title.x=element_blank(),
+                axis.text.x=element_blank(),
+                axis.text.y=element_blank(),
+                axis.ticks.y=element_blank())+
+  xlab("")+
+  coord_polar("y", start=0)+
+            scale_fill_manual("Colour Pie", values = c("W"=rgb(249,250,185,maxColorValue=255),"U"=rgb(14,104,171,maxColorValue=255),"B"=rgb(21,11,0,maxColorValue=255),"R"=rgb(211,32,42,maxColorValue=255),"G"=rgb(0,115,62,maxColorValue=255)))
 col 
 ggsave("col.png")
 
 #deck stats 
-games<-decks%>%filter(priority!="Unfinished"&!is.na(priority))%>%ggplot(aes(played,deck,fill=played))+
+games<-decks%>%filter(type!="Unfinished")%>%ggplot(aes(played,deck,fill=played))+
         geom_bar(stat="identity")+
         scale_fill_viridis_c()+
         theme(legend.position="none")
@@ -34,32 +39,44 @@ ggsave(filename ="played.png",width=3000,height =2255, dpi=300,units="px",device
 # powerlevel
 decks%>%group_by(power)%>%count()
 # playable decks
-decks%>%filter(priority!="Unfinished")%>%count()
+decks%>%filter(type!="Unfinished")%>%count()
 
 
 # win stats 
-
-played <- decks %>% filter(played>0) %>% mutate(win=(won+0.5*draw)/played) %>% arrange(-win)
-LGSplay <- played %>% filter(priority=="LGS") %>%  ggplot(aes(x=lost,y=won,label=commander)) +
+games <- decks %>% filter(played>0) %>% mutate(win=(won+0.5*draw)/played) %>% arrange(-win)
+total <- games %>% group_by(type) %>% summarise(max_played=max(played))
+LGSplay <- games %>% filter(type=="LGS") %>%  
+  ggplot(aes(x=lost,y=won,label=commander)) +
   geom_point(aes(size=played,colour=commander,fill=commander))+
+  geom_abline(slope=0.25,intercept=0)+
+  scale_x_continuous(limits = c(0,total %>% filter(type=="LGS") %>% pull(max_played)))+
+  scale_y_continuous(limits = c(0,total %>% filter(type=="LGS") %>% pull(max_played)))+
   geom_text_repel(aes(label = commander),size=4,vjust = -1,max.overlaps = 21)+
   scale_colour_viridis_d(option="turbo")+
   theme(legend.position = "none")
 LGSplay 
 ggsave("LGSplay.png")
 
-proxied <- played %>% filter(priority=="proxied") %>% 
+proxied_total <- max(games[games$type=="proxied",]$played)
+proxied <- played %>% filter(type=="proxied") %>% 
   ggplot(aes(x=lost,y=won,label=commander)) +
   geom_point(aes(size=played,colour=commander,fill=commander))+
+  geom_abline(slope=0.25,intercept=0)+
+  scale_x_continuous(limits = c(0,total %>% filter(type=="proxied") %>% pull(max_played)))+
+  scale_y_continuous(limits = c(0,total %>% filter(type=="proxied") %>% pull(max_played)))+
   geom_text_repel(aes(label = commander),size=4,vjust = -1,max.overlaps = 21)+
   scale_colour_viridis_d(option="turbo")+
   theme(legend.position = "none")
 proxied
 ggsave("proxied.png")
 
-notEDH <- played %>% filter(priority=="Other") %>% 
+notEDH_total <- max(games[games$type=="Other",]$played)
+notEDH <- played %>% filter(type=="Other") %>% 
   ggplot(aes(x=lost,y=won,label=commander)) +
   geom_point(aes(size=played,colour=commander,fill=commander))+
+  geom_abline(slope=0.25,intercept=0)+
+  scale_x_continuous(limits = c(0,total %>% filter(type=="Other") %>% pull(max_played)))+
+  scale_y_continuous(limits = c(0,total %>% filter(type=="Other") %>% pull(max_played)))+
   geom_text_repel(aes(label = commander),size=4,vjust = -1,max.overlaps = 21)+
   scale_colour_viridis_d(option="turbo")+
   theme(legend.position = "none")
@@ -67,17 +84,21 @@ notEDH
 ggsave("notEDH.png")
 
 # cEDH stats 
-cEDH <- decks %>% filter(priority=='cEDH') %>% mutate(win=(won+0.5*draw)/played) %>% arrange(-win)
+cEDH_total <- max(games[games$type=="cEDH",]$played)
+cEDH <- decks %>% filter(type=='cEDH') %>% mutate(win=(won+0.5*draw)/played) %>% arrange(-win)
 cEDH_results <- ggplot(cEDH, aes(x=lost,y=won,label=commander)) +
                   geom_point(aes(size=played,colour=commander))+
-                  geom_text(check_overlap = TRUE,hjust = 0, nudge_x = -0.2,nudge_y = -0.15)+
-                  scale_colour_viridis_d()+
+                  geom_abline(slope=0.4,intercept=0)+
+                  scale_x_continuous(limits = c(0,total %>% filter(type=="cEDH") %>% pull(max_played)))+
+                  scale_y_continuous(limits = c(0,total %>% filter(type=="cEDH") %>% pull(max_played)))+
+                  geom_text_repel(aes(label = commander),size=4,vjust = -1,max.overlaps = 21)+
+                  scale_colour_viridis_d(option="turbo")+
                   theme(legend.position = "none")
 cEDH_results
 ggsave('cEDH_results.png')
 
 
-buildme<-decks%>%filter(priority=="Unfinished")
+buildme<-decks%>%filter(type=="Unfinished")
 buildme%>%pull(commander)
 # wishlist (This is currently a little behind)
 
@@ -90,5 +111,16 @@ artists
 # decks%>%dplyr::filter(U==T&W==T)%>%pull(commander)
 
 # planes
-planes <- decks %>% separate_rows(plane,sep='&') %>% drop_na() %>% group_by(plane) %>% summarise(n=n()) %>% arrange(-n)
-planes
+planes <- decks %>% separate_rows(plane,sep='&') %>% drop_na() %>% group_by(plane) %>% mutate(n_all=nrow(.)) %>% group_by(n_all,plane)%>% summarise(n=n()) %>% arrange(-n) %>% mutate(prop=n/n_all)
+
+locations <- planes %>% 
+              ggplot(aes(n,1-prop)) +
+              geom_jitter(aes(size=prop,colour=plane))+
+              scale_colour_viridis_d(option="turbo")+
+              geom_text_repel(aes(label = plane),size=4,vjust = -1,max.overlaps = 21)
+locations 
+# themes 
+themes <- decks %>% separate_rows(themes,sep='&') %>% drop_na() %>% group_by(themes) %>% summarise(n=n()) %>% arrange(-n)
+themes
+
+write.csv(decks,"Decks.csv",quote=F,row.names =F)
